ebe886dc2c9454068bb801991376bfc230a9b75c
Bell App Lab <apps@bellapplab.com>
2015-10-18 12:56:56 +0100

    Trying to fix #if directive in Swift

--- Pod/Classes/BLLogger.swift
+++ Pod/Classes/BLLogger.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 public func dLog(@autoclosure message:  () -> String, filename: String = __FILE__, function: String = __FUNCTION__, line: Int = __LINE__) {
-    #if DEBUG
-        NSLog("[\(filename.lastPathComponent):\(line)] \(function) - %@", message())
-        #else
-    #endif
+#if DEBUG
+    NSLog("[\(filename.lastPathComponent):\(line)] \(function) - %@", message())
+#else
+#endif
 }
 public func aLog(message: String, filename: String = __FILE__, function: String = __FUNCTION__, line: Int = __LINE__) {
     NSLog("[\(NSURL(string: filename)?.lastPathComponent):\(line)] \(function) - %@", message)
